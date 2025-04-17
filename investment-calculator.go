c123be4873c291d8da43c1c2706d8ebf1489bd6d
Adarsh Goel <adarshgoel235@gmail.com>
2025-04-18 02:12:20 +0530

    Addition inflation based calculation in the Investment Calculator

--- investment-calculator.go
+++ investment-calculator.go
@@ -48,9 +48,20 @@ func main() {
 	// investmentAmount, years := 1000.0, 10.0
 	// expectedReturnRate := 5.5
 
+	// investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
+
+	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+
+	// fmt.Println(futureValue)
+	// -----------------------------
+
+	//  EXAMPLE 6
+	const inflationRate = 6.5
 	investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
