2009ee685c971bb21043aa08887e1aca288f53f5
Adarsh Goel <adarshgoel235@gmail.com>
2025-04-19 02:21:55 +0530

    Using Sprintf to create strings and hence, output to the console

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,94 +1,107 @@
 package main
 
-// func main() {
-// 	// EXAMPLE 1
-// 	// fmt.Print("Hello World!");
-// 	// -----------------------------
+import (
+	"fmt"
+	"math"
+)
 
-// 	// EXAMPLE 2
-// 	// var investmentAmount = 1000
-// 	// var expectedReturnRate = 5.5
-// 	// var years = 10
+func main() {
+	// EXAMPLE 1
+	// fmt.Print("Hello World!");
+	// -----------------------------
 
-// 	// var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
+	// EXAMPLE 2
+	// var investmentAmount = 1000
+	// var expectedReturnRate = 5.5
+	// var years = 10
 
-// 	// fmt.Println(futureValue)
-// 	// -----------------------------
+	// var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
 
-// 	// EXAMPLE 3
-// 	// var investmentAmount float64 = 1000
-// 	// var expectedReturnRate = 5.5
-// 	// var years float64 = 10
+	// fmt.Println(futureValue)
+	// -----------------------------
 
-// 	// var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	// EXAMPLE 3
+	// var investmentAmount float64 = 1000
+	// var expectedReturnRate = 5.5
+	// var years float64 = 10
 
-// 	// fmt.Println(futureValue)
-// 	// -----------------------------
+	// var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 
-// 	// EXAMPLE 4
-// 	// var investmentAmount float64 = 1000
-// 	// expectedReturnRate := 5.5 // type inferred by Go; explicit type assignment not allowed
-// 	// var years float64 = 10
+	// fmt.Println(futureValue)
+	// -----------------------------
 
-// 	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	// EXAMPLE 4
+	// var investmentAmount float64 = 1000
+	// expectedReturnRate := 5.5 // type inferred by Go; explicit type assignment not allowed
+	// var years float64 = 10
 
-// 	// fmt.Println(futureValue)
-// 	// -----------------------------
+	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 
-// 	// EXAMPLE 5
-// 	// var investmentAmount, years float64 = 1000, 10 // defining multiple variables in one line; cannot specify multiple type assignments
+	// fmt.Println(futureValue)
+	// -----------------------------
 
-// 	// var investmentAmount, years = 1000, 10 // type inferred; can specify multiple values of different types in the same line
+	// EXAMPLE 5
+	// var investmentAmount, years float64 = 1000, 10 // defining multiple variables in one line; cannot specify multiple type assignments
 
-// 	// investmentAmount, years := 1000.0, 10.0
-// 	// expectedReturnRate := 5.5
+	// var investmentAmount, years = 1000, 10 // type inferred; can specify multiple values of different types in the same line
 
-// 	// investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
+	// investmentAmount, years := 1000.0, 10.0
+	// expectedReturnRate := 5.5
 
-// 	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	// investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
 
-// 	// fmt.Println(futureValue)
-// 	// -----------------------------
+	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 
-// 	//  EXAMPLE 6
-// 	// const inflationRate = 6.5
-// 	// investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
+	// fmt.Println(futureValue)
+	// -----------------------------
 
-// 	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-// 	// futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+	//  EXAMPLE 6
+	// const inflationRate = 6.5
+	// investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
 
-// 	// fmt.Println(futureValue)
-// 	// fmt.Println(futureRealValue)
-// 	// -----------------------------
+	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	// futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
 
-// 	// EXAMPLE 7
-// 	const inflationRate = 6.5
+	// fmt.Println(futureValue)
+	// fmt.Println(futureRealValue)
+	// -----------------------------
 
-// 	// var investmentAmount float64 = 1000 // will be overwritten by user input
-// 	// expectedReturnRate, years := 5.5, 10.0
+	// EXAMPLE 7
+	const inflationRate = 2.5
 
-// 	var investmentAmount, expectedReturnRate, years float64
+	// var investmentAmount float64 = 1000 // will be overwritten by user input
+	// expectedReturnRate, years := 5.5, 10.0
 
-// 	fmt.Print("Investment Amount: ")
-// 	fmt.Scan(&investmentAmount)
+	var investmentAmount, expectedReturnRate, years float64
 
-// 	fmt.Print("Expected Return Rate: ")
-// 	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
 
-// 	fmt.Print("Years: ")
-// 	fmt.Scan(&years)
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
-// 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-// 	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
-// 	// fmt.Println("Future Value:", futureValue) // outputs in the same line separated by a blank
-// 	// fmt.Println("Future Value (adjusted after Inflation):", futureRealValue)
+	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
 
-// 	// fmt.Printf("Future Value: %v\n", futureValue) // %v - value of a variable/constant
+	// -------- OUTPUT --------
+	// fmt.Println("Future Value:", futureValue) // outputs in the same line separated by a blank
+	// fmt.Println("Future Value (adjusted after Inflation):", futureRealValue)
 
-// 	fmt.Printf("Future Value: %v\nFuture Value (adjusted after Inflation): %v\n", futureValue, futureRealValue)     // 1708.1444583535929, 909.9730253950707
+	// fmt.Printf("Future Value: %v\n", futureValue) // %v - value of a variable/constant
+	// fmt.Printf("Future Value: %v\nFuture Value (adjusted after Inflation): %v\n", futureValue, futureRealValue) // 1708.1444583535929, 909.9730253950707
 
-// 	fmt.Printf("Future Value: %f\nFuture Value (adjusted after Inflation): %f\n", futureValue, futureRealValue)     // 1708.144458, 909.973025
-// 	fmt.Printf("Future Value: %.2f\nFuture Value (adjusted after Inflation): %.2f\n", futureValue, futureRealValue) // 1708.14, 909.97
-// 	fmt.Printf("Future Value: %.0f\nFuture Value (adjusted after Inflation): %.0f", futureValue, futureRealValue)   // 1708, 910;
-// }
+	// fmt.Printf("Future Value: %f\nFuture Value (adjusted after Inflation): %f\n", futureValue, futureRealValue)     // 1708.144458, 909.973025
+	// fmt.Printf("Future Value: %.2f\nFuture Value (adjusted after Inflation): %.2f\n", futureValue, futureRealValue) // 1708.14, 909.97
+	// fmt.Printf("Future Value: %.0f\nFuture Value (adjusted after Inflation): %.0f", futureValue, futureRealValue)   // 1708, 910;
+
+	// CREATING STRINGS AND THEN OUTPUT
+	formattedFutureValue := fmt.Sprintf("Future value: %.2f\n", futureValue)
+	formattedFutureRealValue := fmt.Sprintf("Future value (adjusted after inflation): %.2f", futureRealValue)
+	fmt.Print(formattedFutureValue, formattedFutureRealValue)
+
+	// fmt.Printf(`Future Value: %.2f
+	// Future Value (adjusted after inflation): %.2f`, futureValue, futureRealValue) // a bit discrepancy in output wih relation to spacing
+}
--- profit_calculator.go
+++ profit_calculator.go
@@ -1,33 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+// func main() {
+// 	var revenue, expenses, taxRate float64
+// 	// var revenue int
+// 	// var expenses, taxRate float64
 
-func main() {
-	var revenue, expenses, taxRate float64
-	// var revenue int
-	// var expenses, taxRate float64
+// 	fmt.Print("Revenue: ")
+// 	fmt.Scan(&revenue)
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+// 	fmt.Print("Expenses: ")
+// 	fmt.Scan(&expenses)
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+// 	fmt.Print("Tax Rate: ")
+// 	fmt.Scan(&taxRate)
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+// 	earningsBeforeTax := revenue - expenses
+// 	// earningsBeforeTax := float64(revenue) - expenses
+// 	profit := earningsBeforeTax * (1 - taxRate/100)
+// 	ratio := earningsBeforeTax / profit
 
-	earningsBeforeTax := revenue - expenses
-	// earningsBeforeTax := float64(revenue) - expenses
-	profit := earningsBeforeTax * (1 - taxRate/100)
-	ratio := earningsBeforeTax / profit
+// 	// ----OUTPUT----
 
-	// fmt.Println("Earnings before Tax", earningsBeforeTax)
-	// fmt.Println("Profit", profit)
-	// fmt.Println("Ratio", ratio)
+// 	// fmt.Println("Earnings before Tax", earningsBeforeTax)
+// 	// fmt.Println("Profit", profit)
+// 	// fmt.Println("Ratio", ratio)
 
-	fmt.Printf("Earnings before Tax (EBT): %.2f\n", earningsBeforeTax)
-	fmt.Printf("Profit: %.2f\n", profit)
-	fmt.Printf("Ratio of EBT to Profit: %.2f", ratio)
-}
+// 	fmt.Printf("Earnings before Tax (EBT): %.2f\n", earningsBeforeTax)
+// 	fmt.Printf("Profit: %.2f\n", profit)
+// 	fmt.Printf("Ratio of EBT to Profit: %.2f", ratio)
+// }
