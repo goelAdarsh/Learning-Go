613d6e4b3d6a24b2aedd489fa76b349014016bec
Adarsh Goel <adarshgoel235@gmail.com>
2025-04-22 05:11:33 +0530

    Updating functions that interact with file system to a generic function

--- bank.go
+++ bank.go
@@ -10,36 +10,39 @@ import (
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error) {
+func getFloatFromFile(fileName string, defaultValue float64) (float64, error) {
 	// data, _ := os.ReadFile(accountBalanceFile) // _ eplicitly tells Go that we know there's a value but we don't wanna use it
 	// if the file doesn't exist, Go doesn't crashes the application unlike other programming languages, instead returns an empty []byte collection, which is then converted into an empty string (below), which is then converted to 0
 	// all functions in Go, are and should be written in such a way that it doesn't crashes the application
 
-	data, err := os.ReadFile(accountBalanceFile)
+	data, err := os.ReadFile(fileName)
 
 	// nil - special value in Go that indicates absence of a useful value
 	if err != nil {
-		return 1000, errors.New("Failed to read balance from the file.") // default balance
+		// return 1000, errors.New("Failed to read balance from the file.") // default balance
+		return defaultValue, errors.New("Failed to read data from the file.") // default balance
 	}
 
-	balanceText := string(data) // []byte --> string
+	valueText := string(data) // []byte --> string
 
-	balance, err := strconv.ParseFloat(balanceText, 64) // string --> float
+	value, err := strconv.ParseFloat(valueText, 64) // string --> float
 	if err != nil {
-		return 1000, errors.New("Failed to parse stored balance value.") // default balance
+		// return 1000, errors.New("Failed to parse stored balance value.") // default balance
+		return defaultValue, errors.New("Failed to parse the stored value.") // default balance
+
 	}
 
-	return balance, nil // nil states no errors
+	return value, nil // nil states no errors
 }
 
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance) // to convert balance from float64 to a string, which can be converted into []byte
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+func writeFloatToFile(value float64, fileName string) {
+	valueText := fmt.Sprint(value) // to convert balance from float64 to a string, which can be converted into []byte
+	os.WriteFile(fileName, []byte(valueText), 0644)
 	// 0644 : 6-Owner(rw-); 4-Group(r--); 4-Others(r--)
 }
 
 func main() {
-	var accountBalance, err = getBalanceFromFile()
+	var accountBalance, err = getFloatFromFile(accountBalanceFile, 1000)
 
 	if err != nil {
 		fmt.Println("ERROR 💥")
@@ -73,7 +76,7 @@ func main() {
 			}
 
 			accountBalance += depositAmount
-			writeBalanceToFile(accountBalance)
+			writeFloatToFile(accountBalance, accountBalanceFile)
 			fmt.Println("Balance updated! New amount:", accountBalance)
 
 		case 3:
@@ -92,7 +95,7 @@ func main() {
 			}
 
 			accountBalance -= withdrawalAmount
-			writeBalanceToFile(accountBalance)
+			writeFloatToFile(accountBalance, accountBalanceFile)
 			fmt.Println("Balance updated! New amount:", accountBalance)
 		default:
 			fmt.Println("Thank You for choosing Go Bank.")
